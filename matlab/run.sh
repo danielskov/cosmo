cea27f067b2d09c85a09d8d2af834237653ebec1
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-08-17 14:29:26 +0200

    add docstring

--- matlab/run.sh
+++ matlab/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+## run.sh
+#  This script must run on the webserver. It launches a Matlab session which
+#  should continue running, until the end of days or the next reboot, whatever
+#  comes first.
+
 /Applications/MATLAB_R2014b.app/bin/matlab -nodesktop -nosplash -nodisplay \
     -nojvm -r "run('file_scanner_mcmc_starter.m')"
