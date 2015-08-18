088599868c853345b5f55fe911a67c9d7a99ef72
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-08-18 16:17:54 +0200

    add first help dialog for sample ID

--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@
         $('.button-collapse').sideNav();
         $('select').material_select();
         //$('select').not('.disabled').material_select();
+        $('.tooltipped').tooltip({delay: 50});
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
