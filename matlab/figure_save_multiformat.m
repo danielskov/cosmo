2fed4994998bb30b81632002d8dd4d664e00db65
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-08-26 16:33:15 +0200

    set current figure, add doc

--- matlab/figure_save_multiformat.m
+++ matlab/figure_save_multiformat.m
@@ -1,7 +1,12 @@
-function figure_save_multiformat(fig, formats)
+function figure_save_multiformat(fig, basename, formats)
 
-%% save figures
-basename = 'plot';
+%% figure_save_multiformat.m
+% save a figure in multiple formats with a single function call.
+% Example: 
+%    figure_save_multiformat(gcf, 'first_plot', ['fig', 'png', 'pdf'])
+
+figure(fig); % set current figure
+
 for i=1:length(formats)
     format = formats(i);
     if strcmp(format, fig)
