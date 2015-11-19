c94d844841e0d04384f1e61b0c779337d7efbf8f
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-11-19 12:01:32 +0100

    all cards full width until offset starts working again

--- index.php
+++ index.php
@@ -136,26 +136,11 @@ if (isset($_GET['wait_id']) && !empty($_GET['wait_id'])) {
                   </div>
                 </div>
               </div>
-              <div class="col s6">
-                <div class="card">
-                  <div class="card-image">
-                <?php 
-                    include('output/' . $_GET['results_id'] . '_Walks.html');
-                ?>
-                  </div>
-                  <div class="card-content">
-                      <h5 class="blue-text">
-                          Results</h5>
-                      <p>The table shows the general statistical results of the
-                      Markov-Chain Monte-Carlo inversion.</p>
-
-                  </div>
-                </div>
-              </div>
             </div>
 
             <div class="row">
-              <div class="col s12 m8 offset-m2">
+                <!--<div class="col s12 m8 offset-m2">-->
+              <div class="col s12">
                 <div class="card">
                   <div class="card-image">
                   <img src="output/<?php
@@ -196,7 +181,8 @@ if (isset($_GET['wait_id']) && !empty($_GET['wait_id'])) {
 
 
             <div class="row">
-              <div class="col s12 m8 offset-m2">
+                <!--<div class="col s12 m8 offset-m2">-->
+              <div class="col s12">
                 <div class="card">
                   <div class="card-image">
                   <img src="output/<?php
@@ -236,7 +222,8 @@ if (isset($_GET['wait_id']) && !empty($_GET['wait_id'])) {
             </div>
 
             <div class="row">
-              <div class="col s12 m8 offset-m2">
+                <!--<div class="col s12 m8 offset-m2">-->
+              <div class="col s12">
                 <div class="card">
                   <div class="card-image">
                   <img src="output/<?php
