df5bcd61d5b92014fd47d7a1d45f696ca6e786ba
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-11-19 11:38:34 +0100

    try different offset

--- index.php
+++ index.php
@@ -120,7 +120,7 @@ if (isset($_GET['wait_id']) && !empty($_GET['wait_id'])) {
             <h2 class="header center orange-text">MCMC inversion results</h2>
 
             <div class="row">
-              <div class="col s12 m8 offset-m2">
+              <div class="col s12 m8 offset-m4">
                 <div class="card">
                   <div class="card-image">
                 <?php 
