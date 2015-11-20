262851997e434ded9a92292e731c6eb8b6f19219
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-11-20 11:19:59 +0100

    add 4 isotope set example data

--- js/history-form-prefiller.js
+++ js/history-form-prefiller.js
@@ -1,6 +1,6 @@
 var PrefillMachine = {
 
-    prefillCorrectly: function() {
+    prefill2isotopes: function() {
         $("#sample_id").val("sample00");
         $("#name").val("John Doe");
         $("#email").val("john@doe.com");
@@ -38,7 +38,54 @@ var PrefillMachine = {
         $("#record_threshold_max").val("4.3");
 
     }
+
+    prefill4isotopes: function() {
+        $("#sample_id").val("sample01");
+        $("#name").val("John Doe");
+        $("#email").val("john@doe.com");
+
+        $("#lat").val("56.08");
+        $("#long").val("10.11");
+
+        $("#be_zobs").val("0.0");
+        $("#al_zobs").val("0.0");
+
+        $("#be_conc").val("1.152e6");
+        $("#al_conc").val("4.940e6");
+        $("#c_conc").val("1.15e5");
+        $("#ne_conc").val("1.2897e7");
+
+        $("#be_uncer").val("2.6");
+        $("#al_uncer").val("4.0");
+        $("#c_uncer").val("2.0");
+        $("#ne_uncer").val("2.0");
+
+        $("#be_prod_spall").val("5.33");
+        $("#al_prod_spall").val("31.1");
+        $("#c_prod_spall").val("14.6");
+        $("#ne_prod_spall").val("20.8");
+
+        $("#be_prod_muons").val("0.106");
+        $("#al_prod_muons").val("0.70");
+        $("#c_prod_muons").val("2.3");
+        $("#ne_prod_muons").val("0.40");
+
+        $("#rock_density").val("2650");
+
+        $("#epsilon_gla_min").val("0.1");
+        $("#epsilon_gla_max").val("1000");
+
+        $("#epsilon_int_min").val("0.1");
+        $("#epsilon_int_max").val("1000");
+
+        $("#t_degla_min").val("10000");
+        $("#t_degla_max").val("12000");
+
+        $("#record_threshold_min").val("3.7");
+        $("#record_threshold_max").val("4.3");
+
+    }
 }
 
 // run the prefiller
-PrefillMachine.prefillCorrectly();
+//PrefillMachine.prefillCorrectly();
